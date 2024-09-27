eec368a4bb3dee0fa599f4dc70ab1180a3a65db5
Reza Zam <rezazam_skol@outlook.com>
2024-09-27 16:57:31 +0200

    Add project files.

---
+++ SkalProj_Datastrukturer_Minne/AnswerToQuestions.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkalProj_Datastrukturer_Minne
+{
+    internal class AnswerToQuestions
+    {
+
+        /* 1. Hur fungerar stacken och heapen? data sparas i stacken genom en ordning att de staplas på varandra,
+        så om vi behöver en viss data och finns data ovan på det så ska de flyttas för att komma åt den specifika data.
+        men däremot data läggs utan ordning på heapen, de är utsridda men man kan ha direkt åtkomst till data där. */
+
+        //Klassen Heap är en refres typ och de variablerna som är deklarerade kommer liggas i Heapen.
+        public class Heap
+        {
+            public int NumberInHeap;
+            public string StringInHeap;
+        }
+
+
+        //Informationen i stacken kommer att raderas efter körningen av metoden StackMetod
+        public string StackMetod(string str)
+        {
+            return str;
+        }
+
+    
+
+        /* 2. Vad är Value Types respektive Reference Types och vad skiljer dem åt?
+        Value types är typer från System.ValuteType som kan ges till olika variabler det med hjälp de som vi kan
+        avgöra vilken typ av variabel vi jobbar med. 
+        Refrence Type ärver från System.Object, refrence type lagras alltid på heapen med skilnad till value types
+        kan både lagras på heapen eller stacken.*/
+
+
+        /* 3. Följande metoder genererar olika svar. Den första returnerar 3, den andra returnerar 4, varför?
+           första metoden använder sig av value typen intoch där har man två olika variabler med samma värde dvs. två variabler
+           med två olika allokering, så när senare y = tilldelas till 4 så påverkar inte x värdet.
+
+        Men i ReturnValue2 metoden används av MyInt som är en refrenstyp och när man sätter y = x så kopieras adressen till objektet
+        x in i y och det betyder att nu både x och y pekar på samma objekt i heapen, och därför värdet alltid kommer värdet uppdaters
+        till nya värdet för de både x och y.*/
+
+
+        //----------------------------------------------------------------------------------
+
+        /* Övning 1: ExamineList()
+         2. När ökar listans kapacitet? Den initiala kapacitet är o från början och den ökar automatiskt
+         medan vi lägger till element.
+
+         3. Med hur mycket ökar kapaciteten? den fördubblar sin kapacitet för att kunna hantera flera element.
+
+         4. Varför ökar inte listans kapacitet i samma takt som element läggs till?
+         för att undvika ineffektiva omallokeringar.
+
+         5. Minskar kapaciteten när element tas bort ur listan? Nej, det reserverade storleken
+          kommer vara samma. Annars listan skulle behöva omallokera sig till en ny array.
+
+         6. När är det då fördelaktigt att använda en egendefinierad array istället för en lista?
+         När man vet antalet av element och de inte kommer att förändras.*/
+
+
+        //---------------------------------------------------------------------------------
+
+        /*Övning 3
+        1. Simulera ännu en gång ICA-kön på papper.Denna gång med en stack.Varför är det inte så smart att använda
+        en stack i det här fallet? För att personen som ställer sig i kön först ska behandlas efter de kommande kunder och det
+        är inte rättvis och effektiv.*/
+
+
+
+
+
+
+
+
+    }
+}
---
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections;
+
+namespace SkalProj_Datastrukturer_Minne
+{
+    class Program
+    {
+        /// <summary>
+        /// The main method, vill handle the menues for the program
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2, 3 ,4, 0) of your choice"
+                    + "\n1. Examine a List"
+                    + "\n2. Examine a Queue"
+                    + "\n3. Examine a Stack"
+                    + "\n4. CheckParenthesis"
+                    + "\n0. Exit the application");
+                char input = ' '; //Creates the character input to be used with the switch-case below.
+                try
+                {
+                    input = Console.ReadLine()![0]; //Tries to set input to the first char in an input line
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter some input!");
+                }
+                switch (input)
+                {
+                    case '1':
+                        ExamineList();
+                        break;
+                    case '2':
+                        ExamineQueue();
+                        break;
+                    case '3':
+                        ExamineStack();
+                        break;
+                    case '4':
+                        CheckParanthesis();
+                        break;
+                    /*
+                     * Extend the menu to include the recursive 
+                     * and iterative exercises.
+                     */
+                    case '0':
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter some valid input (0, 1, 2, 3, 4)");
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Examines the datastructure List
+        /// </summary>
+        static void ExamineList()
+        {
+            /*
+             * Loop this method untill the user inputs something to exit to main menue.
+             * Create a switch statement with cases '+' and '-'
+             * '+': Add the rest of the input to the list (The user could write +Adam and "Adam" would be added to the list)
+             * '-': Remove the rest of the input from the list (The user could write -Adam and "Adam" would be removed from the list)
+             * In both cases, look at the count and capacity of the list
+             * As a default case, tell them to use only + or -
+             * Below you can see some inspirational code to begin working.
+            */
+
+            List<string> theList = new List<string>();
+            Console.WriteLine("Adda till listan med att lägga operator(+) i början av namnet, och (-) för att ta bort");
+
+            bool isAlive = true;
+
+            do
+            {
+                string input = Console.ReadLine();
+                char nav = input[0];
+                string value = input.Substring(1).Trim();
+                Console.WriteLine("Lägga till en person med (+) Ta bort med (-), Avsluta med (0), (h) för huvud meny.");
+
+
+                switch (nav)
+                {
+                    case '+':
+                        theList.Add(value);
+                        break;
+
+                    case '-':
+
+                        if (theList.Contains(value))
+                        {
+                            theList.Remove(value);
+                        }
+                        break;
+
+                    case '0':
+                        Environment.Exit(0);
+                        break;
+
+                    case 'h':
+                    case 'H':
+                        isAlive = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Använd endast (+) för att lägga till (-) för att ta bort eller (0) för att avsluta.");
+                        break;
+
+                }
+
+                Console.WriteLine("Antal personer i listan: " + theList.Count);
+                foreach (string name in theList)
+                {
+                    Console.WriteLine("Person i listan: " + name);
+                }
+                Console.WriteLine();
+
+            }
+            while (isAlive);
+
+
+        }
+
+
+        /// <summary>
+        /// Examines the datastructure Queue
+        /// </summary>
+        static void ExamineQueue()
+        {
+            /*
+             * Loop this method untill the user inputs something to exit to main menue.
+             * Create a switch with cases to enqueue items or dequeue items
+             * Make sure to look at the queue after Enqueueing and Dequeueing to see how it behaves
+            */
+
+
+            Queue<string> queueListName = new Queue<string>();
+            string inputName;
+            bool isAlive = true;
+
+            do
+            {
+                Console.WriteLine("(e) lägger till med Enqueue, (d) tar bort med Dequeue, 'k' för att avsluta");
+                inputName = Console.ReadLine();
+                string inputValue = inputName.Substring(1).Trim();
+                char letter = inputName[0];
+                switch (letter)
+                {
+                    case 'e':
+                        queueListName.Enqueue(inputValue);
+                        break;
+
+                    case 'd':
+                        queueListName.Dequeue();
+                        break;
+
+
+                    case 'k':
+                        isAlive = false;
+                        break;
+
+
+                }
+
+            } while (isAlive);
+
+            foreach (string names in queueListName)
+            {
+                Console.WriteLine($"Namn {names} kvar i kön");
+            }
+            Console.WriteLine();
+
+        }
+
+        /// <summary>
+        /// Examines the datastructure Stack
+        /// </summary>
+        static void ExamineStack()
+        {
+            /*
+             * Loop this method until the user inputs something to exit to main menue.
+             * Create a switch with cases to push or pop items
+             * Make sure to look at the stack after pushing and and poping to see how it behaves
+            */
+
+            Stack stack = new Stack();
+
+            bool isAlive = true;
+
+
+
+            do
+            {
+                Console.WriteLine("(1) För att pusha ett namn på stack" +
+                                 " \n(2) För att pop ett namn från stack" +
+                                  "\n(3) För att skriva ut alla namn på stack" +
+                                 " \n(4) För att få en stäng ömvänd" +
+                                 "\n(5) För att avsluta programmet");
+                string val = Console.ReadLine();
+                string inputStack;
+
+                switch (val)
+                {
+                    case "1":
+                        Console.WriteLine("Skriv en text att lägga till på stacken:");
+                        inputStack = Console.ReadLine();
+                        stack.Push(inputStack);
+                        Console.WriteLine($"{inputStack} har lagts till på stacken.");
+                        Console.WriteLine();
+                        break;
+
+
+
+                    case "2":
+                        stack.Pop();
+                        Console.WriteLine();
+                        break;
+
+                    case "3":
+                        foreach (string item in stack)
+                        {
+                            Console.WriteLine($"Namn {item} finns på stack: ");
+
+                        }
+                        Console.WriteLine();
+                        break;
+
+                    case "4":
+                        inputStack = Console.ReadLine();
+                        ReverseText(inputStack);
+                        Console.WriteLine();
+                        break;
+
+
+                    case "5":
+                        isAlive = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Välj en tal mellan 1 - 5");
+                        break;
+                }
+
+            } while (true);
+
+
+            static void ReverseText(string input)
+            {
+                Stack stack = new Stack();
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    stack.Push(input[i]);
+                }
+
+                Console.WriteLine("Antal bokstäver i strängen: " + stack.Count);
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    Console.Write(stack.Pop());
+
+                }
+
+                Console.WriteLine();
+
+            }
+
+
+        }
+
+        static void CheckParanthesis()
+            {
+                /*
+                 * Use this method to check if the paranthesis in a string is Correct or incorrect.
+                 * Example of correct: (()), {}, [({})],  List<int> list = new List<int>() { 1, 2, 3, 4 };
+                 * Example of incorrect: (()]), [), {[()}],  List<int> list = new List<int>() { 1, 2, 3, 4 );
+                 */
+
+            }
+
+        
+    }
+}
